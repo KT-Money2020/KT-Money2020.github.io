cd08e8717c98b6a515fe8081c86916d38ffc5575
Kevin Tran <mai_le09@yahoo.com>
2019-05-01 11:45:39 -0700

    Added navigation bar for mobile

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function scrollAppear(){
     var introText = document.querySelector('.intro-text');
     var introPosition = introText.getBoundingClientRect().top;
-    var screenPosition = window.innerHeight;
+    var screenPosition = window.innerHeight / 2;
     
     if(introPosition < screenPosition){
         introText.classList.add('intro-appear');
@@ -12,6 +12,24 @@ window.addEventListener('scroll',scrollAppear);
 
 
 
+
+
+
+
+
+
+
+
+function openSlideMenu(){
+         //How far the menu should extend
+         document.getElementById('menuAni').style.width = '200px';
+         document.getElementById('entireContent').style.marginRight = '250px';
+     }
+        
+function closeSlideMenu(){
+         document.getElementById('menuAni').style.width = '0';
+         document.getElementById('entireContent').style.marginRight = '0';
+     }        
 
 
 
